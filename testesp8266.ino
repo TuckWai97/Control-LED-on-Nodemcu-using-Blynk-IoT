330cf46b5e685c0b3110f4242067b852e54bfc63
Yip Tuck Wai <yiptckwai@gmail.com>
2022-05-11 00:20:36 +0800

    Add files via upload
    write program in Arduino sketch and control LEDs on NodeMCU using Blynk IoT(new)

---
+++ testesp8266.ino
@@ -0,0 +1,114 @@
+/*************************************************************
+
+  This is a simple demo of sending and receiving some data.
+  Be sure to check out other examples!
+ *************************************************************/
+
+// Template ID, Device Name and Auth Token are provided by the Blynk.Cloud
+// See the Device Info tab, or Template settings
+#define BLYNK_TEMPLATE_ID "TMPLS4H_zSwu"
+#define BLYNK_DEVICE_NAME "NodeMCU"
+#define BLYNK_AUTH_TOKEN            "wAE4uJpEqs93JRLO54zp6sverW3oiD9Y"
+
+// Comment this out to disable prints and save space
+#define BLYNK_PRINT Serial
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+char auth[] = BLYNK_AUTH_TOKEN;
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "me";
+char pass[] = "Drg2855215@";
+//static const uint8_t D4=2;
+
+
+BlynkTimer timer;
+#define POT A0
+bool eventTrigger = false;
+// This function is called every time the Virtual Pin 0 state changes
+BLYNK_WRITE(V0)
+{
+  // Set incoming value from pin V0 to a variable
+  int value = param.asInt();
+  int invtvalue = !value;
+  // Update state
+  Blynk.virtualWrite(V2, invtvalue);
+  
+  //Serial.println("V0 value: "+ String(value));
+  //digitalWrite(LED_BUILTIN, value);
+  digitalWrite(D0,value);
+}
+
+BLYNK_WRITE(V3)
+{
+  // Set incoming value from pin V0 to a variable
+  int value = param.asInt();
+
+  // Update state
+  Blynk.virtualWrite(V3, value);
+  
+  //Serial.println("V0 value: "+ String(value));
+  //digitalWrite(LED_BUILTIN, value);
+  digitalWrite(D4,value);
+}
+
+
+ 
+// This function sends Arduino's uptime every second to Virtual Pin 2.
+void myTimerEvent()
+{
+  // You can send any value at any time.
+  // Please don't send more than 10 values per second.
+  int pot_adc = analogRead(POT);
+  
+
+  Serial.println("Potentiometer ADC: "+ String(pot_adc));
+ 
+  Blynk.virtualWrite(V1, pot_adc);
+  // You can send any value at any time.
+  // Please don't send more that 10 values per second.
+  
+  if(pot_adc < 100 &&
+     eventTrigger == false) {
+      eventTrigger = true;
+  //Set a flag variable to prevent it from send notifications repeatedly
+      Blynk.logEvent("LIGHT", "Light ADC is less than 100");
+     }
+     else if (pot_adc > 300) {
+      eventTrigger = false;
+     }
+  
+}
+
+void setup()
+{
+  //pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(D4, OUTPUT);
+  pinMode(D0, OUTPUT);
+  pinMode(POT, INPUT); 
+
+  //digitalWrite(LED_BUILTIN, value);
+  // Debug console
+  Serial.begin(115200);
+  
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk.cloud", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+ 
+  // Setup a function to be called every second
+  timer.setInterval(1000L, myTimerEvent);
+}
+
+void loop()
+{
+  Blynk.run();
+  timer.run();
+
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
